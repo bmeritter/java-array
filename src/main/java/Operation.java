6a176922a5b3e41ad074e357c3272930c4a1587f
bmeritter <bme_ritter@foxmail.com>
2017-03-24 16:03:03 +0800

    update

--- src/main/java/Operation.java
+++ src/main/java/Operation.java
@@ -3,11 +3,7 @@ public class Operation {
         int count = 0;
         // 在这里写代码
 
-        for (int a : array) {
-            if (a % 2 == 0) {
-                count++;
-            }
-        }
+
         return count;
     }
 }
